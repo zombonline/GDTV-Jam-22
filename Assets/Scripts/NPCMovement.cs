2198bb2f0ccf46012b26cab125b309892f224bed
DESKTOP-G9P28AG\megaz <megazombie130@gmail.com>
2022-05-22 16:08:26 +0100

    pathfinding

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -1,11 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class NPCMovement : MonoBehaviour
 {
     [SerializeField] float speed;
     public bool isMoving = true;
+    [SerializeField] int xMovementAreaSize, yMovementAreaSize;
 
+    AIPath aiPath;
+    Animator animator;
+
+    IEnumerator PauseAndGenerateNewDestination()
+    {
+        yield return new WaitForSeconds(5f);
+        aiPath.destination = new Vector2(Random.Range(-xMovementAreaSize, xMovementAreaSize), Random.Range(-yMovementAreaSize, yMovementAreaSize));
+    }
+
+    private void Awake()
+    {
+        aiPath = GetComponent<AIPath>();
+        animator = GetComponent<Animator>();
+        
+    }
+    private void Start()
+    {
+        aiPath.destination = new Vector2(Random.Range(-xMovementAreaSize, xMovementAreaSize), Random.Range(-yMovementAreaSize, yMovementAreaSize));
+    }
+
+    private void Update()
+    {
+        if(!isMoving)
+        {
+            return;
+        }
+        if(GetComponent<AIPath>().reachedEndOfPath)
+        {
+            if(animator.GetBool("Walking"))
+            {
+                StartCoroutine(PauseAndGenerateNewDestination());
+            }
+            animator.SetBool("Walking", false);
+        }
+        else
+        {
+            animator.SetBool("Walking", true);
+        }
+    }
 
 }
